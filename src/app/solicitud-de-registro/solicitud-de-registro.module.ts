8be381813de2382d3685b9b34c02cfe9135300aa
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2021-06-08 14:15:43 -0500

    Updated

--- src/app/solicitud-de-registro/solicitud-de-registro.module.ts
+++ src/app/solicitud-de-registro/solicitud-de-registro.module.ts
@@ -7,13 +7,18 @@ import { IonicModule } from '@ionic/angular';
 import { SolicitudDeRegistroPageRoutingModule } from './solicitud-de-registro-routing.module';
 
 import { SolicitudDeRegistroPage } from './solicitud-de-registro.page';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { NgwWowModule } from 'ngx-wow';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    SolicitudDeRegistroPageRoutingModule
+    SolicitudDeRegistroPageRoutingModule,
+    MDBBootstrapModule.forRoot(),
+    NgwWowModule,
+    
   ],
   declarations: [SolicitudDeRegistroPage]
 })
--- src/app/solicitud-de-registro/solicitud-de-registro.page.ts
+++ src/app/solicitud-de-registro/solicitud-de-registro.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgwWowService } from 'ngx-wow';
 
 @Component({
   selector: 'app-solicitud-de-registro',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SolicitudDeRegistroPage implements OnInit {
 
-  constructor() { }
+  constructor(private wowService: NgwWowService) {
+    this.wowService.init(); 
+   }
 
-  ngOnInit() {
+   ngOnInit() {
+    this.wowService.init(); 
   }
 
 }
